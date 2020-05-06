4a242f75fee48f4349a91b75de616f3478ba98cd
bigtony555 <ahtohka@gmail.com>
2020-05-07 01:23:59 +0300

    module 4 done

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,27 @@ export const formatTime = (date) => {
   const minutes = castTimeFormat( date.getMinutes() );
   return `${hours} : ${minutes}`;
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.children[0];
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const render = (container, element, place = `beforeend`) => {
+  // console.log(element)
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
